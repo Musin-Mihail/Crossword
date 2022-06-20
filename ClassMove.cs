243a6b354ff78402149339c0ef82479f702faf3a
Мусин Михаил <musin.mihail@gmail.com>
2022-06-20 15:44:15 +0300

    Первая загрузка

---
+++ ClassMove.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+namespace Crossword
+{
+    internal class ClassMove
+    {
+        public void MoveRight(Border border1, List<Border> listBorder, List<Label> listLabel, List<string> words, Label label)
+        {
+            int numColumn = Grid.GetColumn(border1);
+            int numRow = Grid.GetRow(border1);
+            List<Label> newListLabel = LoopsRight(numColumn, numRow, listBorder, listLabel);
+            if (newListLabel.Count < 16)
+            {
+                if (newListLabel.Count >= 2)
+                {
+                    List<string> listString = new List<string>();
+                    foreach (string word in words)
+                    {
+                        if (word.Length == newListLabel.Count + 1)
+                        {
+                            listString.Add(word);
+                        }
+                    }
+                    Random rnd = new Random();
+                    string newWord = listString[rnd.Next(0, listString.Count - 1)];
+                    label.Content += newWord + "\n";
+                    //MessageBox.Show(newWord);
+                    for (int i = 0; i < newListLabel.Count; i++)
+                    {
+                        newListLabel[i].Content = newWord[i];
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Есть поле больше 15");
+            }
+        }
+        List<Label> LoopsRight(int numColumn, int numRow, List<Border> listBorder, List<Label> listLabel)
+        {
+            List<Label> newListLabel = new List<Label>();
+            for (int i = numColumn; i < 30; i++)
+            {
+                foreach (Border border2 in listBorder)
+                {
+                    if (Grid.GetRow(border2) == numRow)
+                    {
+                        if (Grid.GetColumn(border2) == i)
+                        {
+                            if (border2.Background == Brushes.Transparent || border2.Background == Brushes.Yellow)
+                            {
+                                foreach (Label label in listLabel)
+                                {
+                                    if (Grid.GetRow(label) == numRow)
+                                    {
+                                        if (Grid.GetColumn(label) == i)
+                                        {
+                                            if (i == numColumn)
+                                            {
+                                                newListLabel.Add(label);
+                                            }
+                                            else
+                                            {
+                                                if (label.Content == null)
+                                                {
+                                                    newListLabel.Add(label);
+                                                }
+                                                else
+                                                {
+                                                    return newListLabel;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                return newListLabel;
+                            }
+                        }
+                    }
+                }
+            }
+            return newListLabel;
+        }
+        public bool MoveDown(Border border1, List<Border> listBorder, List<Label> listLabel, List<string> words, Label label)
+        {
+            int numColumn = Grid.GetColumn(border1);
+            int numRow = Grid.GetRow(border1);
+            List<Label> newListLabel = LoopsDown(numColumn, numRow, listBorder, listLabel);
+            if (newListLabel.Count >= 2)
+            {
+                List<string> listString = new List<string>();
+                foreach (string word in words)
+                {
+                    if (word.Length == newListLabel.Count + 1)
+                    {
+                        listString.Add(word);
+                    }
+                }
+                List<string> tempListString = new List<string>();
+                for (int i = 0; i < newListLabel.Count; i++)
+                {
+                    if (newListLabel[i].Content != null)
+                    {
+                        foreach (string item in listString)
+                        {
+                            string tempString = newListLabel[i].Content.ToString();
+                            string tempString2 = item[i].ToString();
+                            if (tempString2 == tempString)
+                            {
+                                tempListString.Add(item);
+                            }
+                        }
+                        if (tempListString.Count > 0)
+                        {
+                            listString = new List<string>(tempListString);
+                            tempListString.Clear();
+                        }
+                        else
+                        {
+                            border1.Background = Brushes.Blue;
+                            //MessageBox.Show("Нет подходящего слова");
+                            border1.Background = Brushes.Yellow;
+                            return false;
+                        }
+                    }
+                }
+                Random rnd = new Random();
+                string newWord = listString[rnd.Next(0, listString.Count - 1)];
+                label.Content += newWord + "\n";
+                //MessageBox.Show(newWord);
+                for (int i = 0; i < newListLabel.Count; i++)
+                {
+                    newListLabel[i].Content = newWord[i];
+                }
+            }
+            return true;
+        }
+        List<Label> LoopsDown(int numColumn, int numRow, List<Border> listBorder, List<Label> listLabel)
+        {
+            List<Label> newListLabel = new List<Label>();
+            for (int i = numRow; i < 30; i++)
+            {
+                foreach (Border border2 in listBorder)
+                {
+                    if (Grid.GetColumn(border2) == numColumn)
+                    {
+                        if (Grid.GetRow(border2) == i)
+                        {
+                            if (border2.Background == Brushes.Transparent || border2.Background == Brushes.Yellow)
+                            {
+                                border2.Background = Brushes.Transparent;
+                                foreach (Label label in listLabel)
+                                {
+                                    if (Grid.GetColumn(label) == numColumn)
+                                    {
+                                        if (Grid.GetRow(label) == i)
+                                        {
+
+                                            newListLabel.Add(label);
+
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                return newListLabel;
+                            }
+                        }
+                    }
+                }
+            }
+            return newListLabel;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace Crossword
+{
+    //Удалить жёлтые отметки. Установка цифр в начале слов. скриншоты заполненной и пустой сетки.
+    public partial class MainWindow : Window
+    {
+        List<Label> listLabel = new List<Label>();
+        List<Border> listBorder = new List<Border>();
+        bool number = false;
+        string Data = File.ReadAllText("dict.txt");
+        List<string> words;
+        ClassMove classMove = new ClassMove();
+        List<Border> yellowListborder = new List<Border>();
+        List<Border> saveYellowListborder = new List<Border>();
+        List<Border> saveWhiteListborder = new List<Border>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            words = new List<string>(Data.Split('\n'));
+            CreateGrid();
+        }
+        void CreateGrid()
+        {
+            for (int y = 0; y < 30; y++)
+            {
+                for (int x = 0; x < 30; x++)
+                {
+                    Label myLabel = CreateLabel();
+                    listLabel.Add(myLabel);
+                    TheGrid.Children.Add(myLabel);
+                    Grid.SetColumn(myLabel, x);
+                    Grid.SetRow(myLabel, y);
+
+                    Border myBorder = CreateBorder();
+                    listBorder.Add(myBorder);
+                    TheGrid.Children.Add(myBorder);
+                    Grid.SetColumn(myBorder, x);
+                    Grid.SetRow(myBorder, y);
+                }
+            }
+        }
+        Border CreateBorder()
+        {
+            Border myBorder = new Border();
+            myBorder.Background = Brushes.Black;
+            myBorder.BorderBrush = Brushes.Black;
+            myBorder.BorderThickness = new Thickness(0.5);
+            myBorder.MouseEnter += new MouseEventHandler(MoveChangeColor);
+            myBorder.MouseDown += new MouseButtonEventHandler(ClickChangeColor);
+            return myBorder;
+        }
+        Label CreateLabel()
+        {
+            Label myLabel = new Label();
+            myLabel.HorizontalAlignment = HorizontalAlignment.Center;
+            myLabel.VerticalAlignment = VerticalAlignment.Center;
+            return myLabel;
+        }
+        private void MoveChangeColor(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed && number == false)
+            {
+                ChangeColorBlackWhite(sender);
+            }
+        }
+        private void ClickChangeColor(object sender, MouseButtonEventArgs e)
+        {
+            if (number == false)
+            {
+                ChangeColorBlackWhite(sender);
+            }
+            else
+            {
+                ChangeColorWhiteYellow(sender);
+            }
+        }
+        void ChangeColorBlackWhite(object sender)
+        {
+            Border myBorder = (Border)sender;
+            if (myBorder.Background == Brushes.Transparent)
+            {
+                myBorder.Background = Brushes.Black;
+            }
+            else
+            {
+                myBorder.Background = Brushes.Transparent;
+            }
+        }
+        void ChangeColorWhiteYellow(object sender)
+        {
+            Border myBorder = (Border)sender;
+            if (myBorder.Background == Brushes.Transparent)
+            {
+                myBorder.Background = Brushes.Yellow;
+            }
+            else if (myBorder.Background == Brushes.Yellow)
+            {
+                myBorder.Background = Brushes.Transparent;
+            }
+        }
+        private void Button_ClickNumber(object sender, RoutedEventArgs e)
+        {
+            number = true;
+            Testing.Text = "Назначай начало слов";
+        }
+        private void Button_ClickGen(object sender, RoutedEventArgs e)
+        {
+
+            bool check = false;
+            yellowListborder.Clear();
+            foreach (var border in listBorder)
+            {
+                if (border.Background == Brushes.Yellow)
+                {
+                    yellowListborder.Add(border);
+                }
+            }
+            foreach (var label in listLabel)
+            {
+                label.Content = null;
+            }
+            int count = 0;
+            int countGen = 0;
+            try
+            {
+                countGen = Int32.Parse(CountGen.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Вводите только цифры в количество генераций");
+            }
+            while (count < countGen && yellowListborder.Count > 0)
+            {
+                Worlds.Content = "";
+                count++;
+                foreach (var border in yellowListborder)
+                {
+                    if (border.Background == Brushes.Yellow)
+                    {
+                        classMove.MoveRight(border, listBorder, listLabel, words, Worlds);
+                    }
+                }
+                foreach (var border in yellowListborder)
+                {
+                    if (border.Background == Brushes.Yellow)
+                    {
+                        check = classMove.MoveDown(border, listBorder, listLabel, words, Worlds);
+                        if (check == false)
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (check == false)
+                {
+                    foreach (var border in yellowListborder)
+                    {
+                        border.Background = Brushes.Yellow;
+                    }
+                    foreach (var label in listLabel)
+                    {
+                        label.Content = null;
+                    }
+                }
+                else
+                {
+                    foreach (var border in yellowListborder)
+                    {
+                        border.Background = Brushes.Transparent;
+                    }
+                    yellowListborder.Clear();
+                }
+            }
+            if (check == false)
+            {
+                MessageBox.Show("Генерация не удалась");
+            }
+            else
+            {
+                MessageBox.Show("Генерация прошла успешно");
+
+            }
+            //foreach (var border in yellowListborder)
+            //{
+            //    if (check == true)
+            //    {
+            //        border.Background = Brushes.Transparent;
+            //    }
+            //    else
+            //    {
+            //        border.Background = Brushes.Yellow;
+            //    }
+            //}
+        }
+        private void Button_ClickGrid(object sender, RoutedEventArgs e)
+        {
+            number = false;
+            Testing.Text = "Рисуй сетку";
+        }
+        private void Button_ClickSaveGrid(object sender, RoutedEventArgs e)
+        {
+            foreach (var border in listBorder)
+            {
+                if (border.Background == Brushes.Transparent)
+                {
+                    saveWhiteListborder.Add(border);
+                }
+                else if (border.Background == Brushes.Yellow)
+                {
+                    saveYellowListborder.Add(border);
+                }
+            }
+            SaveGrid.Content = "Сетка сохранена";
+        }
+        private void Button_ClickLoadGrid(object sender, RoutedEventArgs e)
+        {
+            foreach (var label in listLabel)
+            {
+                label.Content = "";
+            }
+            foreach (var border in listBorder)
+            {
+                border.Background = Brushes.Black;
+            }
+            foreach (var border in saveWhiteListborder)
+            {
+                border.Background = Brushes.Transparent;
+            }
+            foreach (var border in saveYellowListborder)
+            {
+                border.Background = Brushes.Yellow;
+            }
+            SaveGrid.Content = "Сетка загружена";
+        }
+    }
+}
