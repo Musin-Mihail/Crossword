510e2b9ab81254e90e9696cf4a9ab4625c885d61
Мусин Михаил <musin.mihail@gmail.com>
2025-06-28 04:28:57 +0300

    Окно выбора словарей.

--- DictionariesSelection.xaml.cs
+++ DictionariesSelection.xaml.cs
@@ -1,53 +1,24 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Windows;
-using System.Windows.Controls;
+using Crossword.ViewModel;
 
 namespace Crossword;
 
 public partial class DictionariesSelection : Window
 {
-    public bool Ready;
-    public readonly List<string> SelectedDictionaries = new();
+    public bool Ready { get; private set; }
+    public List<string> SelectedDictionaries { get; private set; } = new();
 
     public DictionariesSelection()
     {
         InitializeComponent();
-        var dictionariesPaths = GetAllDictionaryPaths.Get();
-        foreach (var path in dictionariesPaths)
+        var viewModel = new DictionariesSelectionViewModel();
+        DataContext = viewModel;
+        viewModel.CloseRequested += () =>
         {
-            var countWords = File.ReadAllLines(path).Length;
-            var name = Path.GetFileNameWithoutExtension(path);
-            var canvas = CreateLineDictionary.CreateLine(name, countWords);
-            ItemsControl.Items.Add(canvas);
-        }
-    }
-
-    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-    {
-        for (var i = 0; i < ItemsControl.Items.Count; i++)
-        {
-            var uiElement = (Canvas)ItemsControl.ItemContainerGenerator.ContainerFromIndex(i);
-            var textBox = (TextBox)uiElement.Children[2];
-            if (textBox.Text.Length > 0)
-            {
-                var count = int.Parse(textBox.Text);
-                if (count > 0)
-                {
-                    var countWords = (Label)uiElement.Children[3];
-                    var count2 = int.Parse(countWords.Content.ToString() ?? string.Empty);
-                    if (count > count2)
-                    {
-                        count = count2;
-                    }
-
-                    var label = (Label)uiElement.Children[1];
-                    SelectedDictionaries.Add(label.Content + ";" + count);
-                }
-            }
-        }
-
-        Ready = true;
-        DialogResult = true;
+            SelectedDictionaries = viewModel.SelectionResult;
+            Ready = true;
+            DialogResult = true;
+        };
     }
 }
---
+++ ViewModel/DictionaryInfoViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Crossword.ViewModel;
+
+public class DictionaryInfoViewModel : INotifyPropertyChanged
+{
+    private string _selectedWordCount = "0";
+
+    public string Name { get; set; }
+    public int TotalWordCount { get; set; }
+
+    public string SelectedWordCount
+    {
+        get => _selectedWordCount;
+        set
+        {
+            if (value.All(char.IsDigit))
+            {
+                _selectedWordCount = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public DictionaryInfoViewModel(string name, int totalWordCount)
+    {
+        Name = name;
+        TotalWordCount = totalWordCount;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+public class DictionariesSelectionViewModel
+{
+    public ObservableCollection<DictionaryInfoViewModel> Dictionaries { get; } = new();
+    public List<string> SelectionResult { get; } = new();
+    public ICommand AcceptCommand { get; }
+    public event Action? CloseRequested;
+
+    public DictionariesSelectionViewModel()
+    {
+        var dictionariesPaths = Directory.GetFiles("Dictionaries/").ToList();
+        foreach (var path in dictionariesPaths)
+        {
+            var countWords = File.ReadAllLines(path).Length;
+            var name = Path.GetFileNameWithoutExtension(path);
+            Dictionaries.Add(new DictionaryInfoViewModel(name, countWords));
+        }
+
+        AcceptCommand = new RelayCommand(AcceptSelection);
+    }
+
+    private void AcceptSelection(object? parameter)
+    {
+        foreach (var dictInfo in Dictionaries)
+        {
+            if (!string.IsNullOrEmpty(dictInfo.SelectedWordCount) && int.TryParse(dictInfo.SelectedWordCount, out int count) && count > 0)
+            {
+                var finalCount = count > dictInfo.TotalWordCount ? dictInfo.TotalWordCount : count;
+                SelectionResult.Add($"{dictInfo.Name};{finalCount}");
+            }
+        }
+
+        CloseRequested?.Invoke();
+    }
+}
